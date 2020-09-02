5763c3c3895480c3516c3def951ad82db379b773
ko-kamenashi <kamenashi0917@gmail.com>
2020-09-02 16:21:27 +0900

    feature: update scraping method

--- scraping.py
+++ scraping.py
@@ -2,17 +2,38 @@ import re
 
 from requests_html import HTMLSession
 
-def main():
+TARGET_URL = 'https://***'
+
+def scrape_target_tag():
     session  = HTMLSession()
-    response = session.get('***')
+    response = session.get(TARGET_URL)
 
-    response.html.render()
+    response.html.render(wait=30)
 
-    rows = response.html.find('***')
+    return response.html.find('***')
+
+def scrape_files_num(rows):
+    files_num = []
 
     for row in rows:
-        # r = re.search('[0-9]{5}', row.full_text)
-        print(row.html) # TODO: delete before commit
+        _row: str = row.html
+        file_num   = extract_file_num(_row)
+
+        if file_num:
+            files_num.append(file_num.string)
+            print(file_num.string) # TODO: delete before commit
+
+    return files_num
+
+def extract_file_num(row):
+        return re.search('[0-9]{5}', row)
+
+def download_files(files_num):
+    return files_num
+
+def main():
+    rows     = scrape_target_tag()
+    files_num = scrape_files_num(rows)
 
 if __name__ == "__main__":
     try:
