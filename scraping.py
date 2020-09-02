977df030a92c9ad11ec0ae1573a2dbb44bc839d5
ko-kamenashi <kamenashi0917@gmail.com>
2020-09-02 16:22:43 +0900

    Revert "feature: update scraping method"
    This reverts commit 5763c3c3895480c3516c3def951ad82db379b773.

--- scraping.py
+++ scraping.py
@@ -2,38 +2,17 @@ import re
 
 from requests_html import HTMLSession
 
-TARGET_URL = 'https://***'
-
-def scrape_target_tag():
+def main():
     session  = HTMLSession()
-    response = session.get(TARGET_URL)
+    response = session.get('***')
 
-    response.html.render(wait=30)
+    response.html.render()
 
-    return response.html.find('***')
-
-def scrape_files_num(rows):
-    files_num = []
+    rows = response.html.find('***')
 
     for row in rows:
-        _row: str = row.html
-        file_num   = extract_file_num(_row)
-
-        if file_num:
-            files_num.append(file_num.string)
-            print(file_num.string) # TODO: delete before commit
-
-    return files_num
-
-def extract_file_num(row):
-        return re.search('[0-9]{5}', row)
-
-def download_files(files_num):
-    return files_num
-
-def main():
-    rows     = scrape_target_tag()
-    files_num = scrape_files_num(rows)
+        # r = re.search('[0-9]{5}', row.full_text)
+        print(row.html) # TODO: delete before commit
 
 if __name__ == "__main__":
     try:
